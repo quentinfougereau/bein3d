3e8e7b0b7e039572856618e9101c3418bfc07c63
Quentin <quentin.fougereau@gmail.com>
2015-02-18 19:17:57 +0100

    ajout formulaire page de profil

--- application/controllers/ProfilController.php
+++ application/controllers/ProfilController.php
@@ -10,7 +10,21 @@ class ProfilController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        
+        //if (($_POST['name'] != null) && ($_POST['fname'] != null) && ($_POST['adress'] != null) && ($_POST['sexe'] != null)) {
+        if(isset($_POST['name'])){
+         $this->view->name = $_POST['name'];
+        $this->view->fname=$_POST['fname'];
+        $this->view->adress=$_POST['adress'];
+        $this->view->sexe=$_POST['choixsexe'];
+        
+        var_dump($_POST);
+        
+        }else {
+            $this->view->error="caca";
+        }
+        
+                
     }
 
 
