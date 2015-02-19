e5ed6eb842d71129dd339ed2eb17c22232f890e9
Quentin <quentin.fougereau@gmail.com>
2015-02-19 19:11:11 +0100

    ajout du formulaire ajax-form

--- application/controllers/ProfilController.php
+++ application/controllers/ProfilController.php
@@ -1,32 +1,30 @@
 <?php
 
-class ProfilController extends Zend_Controller_Action
-{
+class ProfilController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        
+    public function indexAction() {
+
         //if (($_POST['name'] != null) && ($_POST['fname'] != null) && ($_POST['adress'] != null) && ($_POST['sexe'] != null)) {
-        if(isset($_POST['name'])){
-         $this->view->name = $_POST['name'];
-        $this->view->fname=$_POST['fname'];
-        $this->view->adress=$_POST['adress'];
-        $this->view->sexe=$_POST['choixsexe'];
-        
-        var_dump($_POST);
-        
-        }else {
-            $this->view->error="caca";
+        if (isset($_POST['name'])) {
+            $this->view->name = $_POST['name'];
+            $this->view->fname = $_POST['fname'];
+            $this->view->adress = $_POST['adress'];
+            $this->view->sexe = $_POST['sexe'];
+            var_dump($_POST);
+        } else {
+            $this->view->error = "Erreur";
         }
-        
-                
+
+        $request = $this->getRequest();
+
+        if ($request->isXmlHttpRequest()) { // If it's ajax call
+            $data = $request->getPost('data');
+            var_dump($data);
+        }
     }
 
-
 }
-
